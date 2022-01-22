3effc2352da5f5ce5de7743ba419aa15f95aefbb
Logan Reynolds <gizmordo@gmail.com>
2022-01-22 17:59:37 -0500

    Abstracted out powered item code

--- src/main/java/com/github/kydzombie/stapitest/item/PoweredItem.java
+++ src/main/java/com/github/kydzombie/stapitest/item/PoweredItem.java
@@ -1,14 +1,16 @@
 package com.github.kydzombie.stapitest.item;
 
 import com.github.kydzombie.stapitest.util.machine.power.ItemPowerStorage;
-import jdk.internal.org.jline.utils.Colors;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.TextRenderer;
+import net.minecraft.client.render.entity.ItemRenderer;
+import net.minecraft.client.texture.TextureManager;
 import net.minecraft.item.ItemInstance;
-import net.modificationstation.stationapi.api.client.gui.CustomTooltipProvider;
 import net.modificationstation.stationapi.api.registry.Identifier;
 import net.modificationstation.stationapi.api.template.item.TemplateItemBase;
-import net.modificationstation.stationapi.api.util.Colours;
+import org.lwjgl.opengl.GL11;
 
-public abstract class PoweredItem extends TemplateItemBase implements ItemPowerStorage, CustomTooltipProvider {
+public abstract class PoweredItem extends TemplateItemBase implements ItemPowerStorage {
     public PoweredItem(Identifier identifier, int maxPower) {
         super(identifier);
         this.setMaxStackSize(1);
@@ -27,7 +29,7 @@ public abstract class PoweredItem extends TemplateItemBase implements ItemPowerS
 
     @Override
     public int charge(ItemInstance item, int chargeAmount, boolean simulate) {
-        chargeAmount = Math.min(chargeAmount, item.getDamage());
+        chargeAmount = Math.min(item.getDamage(), chargeAmount);
         if (!simulate) {
             item.setDamage(item.getDamage() - chargeAmount);
         }
@@ -46,10 +48,22 @@ public abstract class PoweredItem extends TemplateItemBase implements ItemPowerS
     }
 
     @Override
-    public String[] getTooltip(ItemInstance itemInstance, String originalTooltip) {
-        return new String[]{
-                originalTooltip,
-                "" + Colours.RED + ((ItemPowerStorage) itemInstance.getType()).getCurrentPower(itemInstance) + Colours.WHITE + "/" + Colours.DARK_AQUA + ((ItemPowerStorage) itemInstance.getType()).getMaxPower(itemInstance) + Colours.WHITE + " power stored"
-        };
+    public void renderItemOverlay(ItemRenderer itemRenderer, int itemX, int itemY, ItemInstance itemInstance, TextRenderer textRenderer, TextureManager textureManager) {
+        int barLength = (int)Math.round((((double)getCurrentPower(itemInstance) / (double)getMaxPower(itemInstance)) * 13));
+        int colourOffset = 255-(int)Math.round((((double)getCurrentPower(itemInstance) / (double)getMaxPower(itemInstance)) * 225));
+        GL11.glDisable(2896);
+        GL11.glDisable(2929);
+        GL11.glDisable(3553);
+        Tessellator var8 = Tessellator.INSTANCE;
+        int barColour = Math.max((colourOffset/8) - 130, 100) << 16 | (233-colourOffset) << 8 | 255-(colourOffset/4);
+        int backgroundColour = (255 - colourOffset) / 4 << 16 | 16128;
+        int barOffset = 0;
+        method_1485(var8, itemX + 2, itemY + 13 - barOffset, 13, 2, 0);
+        method_1485(var8, itemX + 2, itemY + 13 - barOffset, 12, 1, backgroundColour);
+        method_1485(var8, itemX + 2, itemY + 13 - barOffset, barLength, 1, barColour);
+        GL11.glEnable(3553);
+        GL11.glEnable(2896);
+        GL11.glEnable(2929);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
--- src/main/java/com/github/kydzombie/stapitest/item/tool/ElectricPickaxe.java
+++ src/main/java/com/github/kydzombie/stapitest/item/tool/ElectricPickaxe.java
@@ -1,11 +1,28 @@
 package com.github.kydzombie.stapitest.item.tool;
 
 import net.minecraft.block.BlockBase;
+import net.minecraft.item.ItemInstance;
 import net.minecraft.item.tool.ToolMaterial;
+import net.modificationstation.stationapi.api.item.tool.ToolLevel;
+import net.modificationstation.stationapi.api.item.tool.ToolMaterialFactory;
 import net.modificationstation.stationapi.api.registry.Identifier;
 
-public class ElectricPickaxe extends PoweredTool {
+public class ElectricPickaxe extends PoweredTool implements ToolLevel {
+
+    private static ToolMaterial empowered;
+
     public ElectricPickaxe(Identifier identifier, ToolMaterial toolMaterial, int maxPower) {
         super(identifier, 0, toolMaterial, new BlockBase[]{}, maxPower);
+        empowered = ToolMaterialFactory.create("empoweredPickaxe", 2, 250, 10.0F, 2);
+    }
+
+    @Override
+    public ToolMaterial getMaterial(ItemInstance item) {
+        if (getCurrentPower(item) >= 5) {
+            return empowered;
+        }
+        else {
+            return toolMaterial;
+        }
     }
 }
