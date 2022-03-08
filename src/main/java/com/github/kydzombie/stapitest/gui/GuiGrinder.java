9d23e578e9c81dff9101bb4a34a9e595a51b94d4
Logan <gizmordo@gmail.com>
2022-03-08 08:48:05 -0500

    Gui refactoring

--- src/main/java/com/github/kydzombie/stapitest/gui/GuiGrinder.java
+++ src/main/java/com/github/kydzombie/stapitest/gui/GuiGrinder.java
@@ -5,20 +5,9 @@ import com.github.kydzombie.stapitest.tileentity.TileGrinder;
 import net.minecraft.entity.player.PlayerInventory;
 import org.lwjgl.opengl.GL11;
 
-public class GuiGrinder extends GuiMachine {
+public class GuiGrinder extends ProcessingGui {
 
     public GuiGrinder(PlayerInventory playerInventory, TileGrinder tileEntity) {
         super(new ContainerGrinder(playerInventory, tileEntity), tileEntity, "Grinder");
-    }
-
-    public void renderContainerBackground(float tickDelta) {
-        int var2 = this.minecraft.textureManager.getTextureId("/assets/stapitest/stationapi/gui/basic_machine.png");
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.minecraft.textureManager.bindTexture(var2);
-        int var3 = (this.width - this.containerWidth) / 2;
-        int var4 = (this.height - this.containerHeight) / 2;
-        this.blit(var3, var4, 0, 0, this.containerWidth, this.containerHeight);
-        int var5 = ((TileGrinder)tileEntity).getCookTimeDelta(24);
-        this.blit(var3 + 79, var4 + 34, 176, 14, var5 + 1, 16);
     }
 }
---
+++ src/main/java/com/github/kydzombie/stapitest/gui/ProcessingGui.java
@@ -0,0 +1,24 @@
+package com.github.kydzombie.stapitest.gui;
+
+import com.github.kydzombie.stapitest.tileentity.ProcessingMachine;
+import com.github.kydzombie.stapitest.tileentity.TileMachine;
+import net.minecraft.container.ContainerBase;
+import org.lwjgl.opengl.GL11;
+
+public abstract class ProcessingGui extends GuiMachine {
+    public ProcessingGui(ContainerBase container, TileMachine tileEntityMachine, String titleText) {
+        super(container, tileEntityMachine, titleText);
+    }
+
+    @Override
+    public void renderContainerBackground(float tickDelta) {
+        int var2 = this.minecraft.textureManager.getTextureId("/assets/stapitest/stationapi/gui/basic_machine.png");
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        this.minecraft.textureManager.bindTexture(var2);
+        int var3 = (this.width - this.containerWidth) / 2;
+        int var4 = (this.height - this.containerHeight) / 2;
+        this.blit(var3, var4, 0, 0, this.containerWidth, this.containerHeight);
+        int var5 = ((ProcessingMachine)tileEntity).getCookTimeDelta(24);
+        this.blit(var3 + 79, var4 + 34, 176, 14, var5 + 1, 16);
+    }
+}
