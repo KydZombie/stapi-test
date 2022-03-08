9a7ca7c27f5409b5bb5f0b5790af5b71973f68f7
Logan <gizmordo@gmail.com>
2022-03-08 14:12:49 -0500

    Minor recipe fixes

--- src/main/java/com/github/kydzombie/stapitest/events/init/RecipeListener.java
+++ src/main/java/com/github/kydzombie/stapitest/events/init/RecipeListener.java
@@ -28,7 +28,7 @@ public class RecipeListener {
 
             CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.wrench), "I I", " G ", " I ", 'I', ItemBase.ironIngot, 'G', StapiTest.goldGear);
 
-            CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.battery, 1, StapiTest.batteryItem.getDurability()), " C ", "PRP", "PRP", 'C', StapiTest.powerCable, 'P', StapiTest.ironPlate, 'R', ItemBase.redstoneDust);
+            CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.batteryItem, 1, StapiTest.batteryItem.getDurability()), " C ", "PRP", "PRP", 'C', StapiTest.powerCableItem, 'P', StapiTest.ironPlate, 'R', ItemBase.redstoneDust);
 
             CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.powerToolHandle), "XGX", "XBX", "XVX", 'X', StapiTest.ironPlate, 'G', StapiTest.goldGear, 'B', new ItemInstance(StapiTest.battery, 1, -1), 'V', StapiTest.goldPlate);
             CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.drillHead), " XX", "XRX", "RX ", 'X', StapiTest.ironPlate, 'R', StapiTest.goldGear);
@@ -38,15 +38,16 @@ public class RecipeListener {
 
             CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.drill, 1, StapiTest.drill.getDurability()), "PD", "HP", 'P', StapiTest.ironPlate, 'D', StapiTest.drillHead, 'H', StapiTest.powerToolHandle);
             CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.saw, 1, StapiTest.drill.getDurability()), "PS", "HP", 'P', StapiTest.ironPlate, 'S', StapiTest.sawHead, 'H', StapiTest.powerToolHandle);
-            CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.diamondDrill, 1, StapiTest.drill.getDurability()), "PD", "HP", 'P', StapiTest.goldPlate, 'D', StapiTest.diamondDrillHead, 'H', StapiTest.powerToolHandle);
-            CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.diamondSaw, 1, StapiTest.drill.getDurability()), "PS", "HP", 'P', StapiTest.goldPlate, 'S', StapiTest.diamondSawHead, 'H', StapiTest.powerToolHandle);
+            CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.diamondDrill, 1, StapiTest.diamondDrill.getDurability()), "PD", "HP", 'P', StapiTest.goldPlate, 'D', StapiTest.diamondDrillHead, 'H', StapiTest.powerToolHandle);
+            CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.diamondSaw, 1, StapiTest.diamondSaw.getDurability()), "PS", "HP", 'P', StapiTest.goldPlate, 'S', StapiTest.diamondSawHead, 'H', StapiTest.powerToolHandle);
 
             CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.generator), " P ", "PBP", "PFP", 'P', StapiTest.ironPlate, 'B', new ItemInstance(StapiTest.battery, 1, -1), 'F', BlockBase.FURNACE);
             CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.electricFurnace), "PPP", "PFP", "PPP", 'P', StapiTest.ironPlate, 'F', BlockBase.FURNACE);
             CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.grinder), "PPP", "PSP", "PPP", 'P', StapiTest.ironPlate, 'S', StapiTest.sawHead);
             CraftingRegistry.addShapedRecipe(new ItemInstance(StapiTest.press), "PGP", "LLL", "PFP", 'P', StapiTest.ironPlate, 'G', StapiTest.goldGear, 'L', StapiTest.goldPlate, 'F', StapiTest.electricFurnace);
         }
-//        else if (event.recipeId.equals(RecipeRegisterEvent.Vanilla.CRAFTING_SHAPELESS.type())) {
-//        }
+        else if (event.recipeId.equals(RecipeRegisterEvent.Vanilla.CRAFTING_SHAPELESS.type())) {
+            CraftingRegistry.addShapelessRecipe(new ItemInstance(StapiTest.battery), new ItemInstance(StapiTest.batteryItem, 1, -1));
+        }
     }
 }
